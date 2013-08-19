d6e4bd47706528733209dc2e57f62fa60a56999c
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-19 10:51:44 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flapjack::Application.config.secret_key_base = '5e31c2b761e21390d9bf31ea9e34fc5bdf43a89916a829833ea836cfe646f3e77039232a37ddc6e2211d45f347f1004dad23d07de73c1379c1083ec4e6289709'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flapjack::Application.config.session_store :cookie_store, key: '_flapjack_session'
