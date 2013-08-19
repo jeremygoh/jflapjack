846e133a44008d335e6906d466f45df0fdd18faa
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-19 16:56:18 +0100

    some styling added to home post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :square => "250x250>"}
 
 end
