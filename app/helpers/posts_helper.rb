259f8a9b9e0ee9c415bb3f28afc8d5cd02b99199
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-19 18:06:07 +0100

    first working dynamic div sizing

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+	def get_size_mulitplier(post)
+  	(post.time * 0.000011574).round(3)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130819122701) do
+ActiveRecord::Schema.define(version: 20130819160733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20130819122701) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.integer  "time"
   end
 
   create_table "users", force: true do |t|
