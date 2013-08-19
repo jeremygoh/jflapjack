654affe9c0aa7f999b8597275351c57abc795c74
Jeremy Goh <jg485host@gmail.com>
2013-08-19 21:15:56 +0100

    fixed spelling of multiplier and change before create duration to time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130819152501) do
+ActiveRecord::Schema.define(version: 20130819160733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
