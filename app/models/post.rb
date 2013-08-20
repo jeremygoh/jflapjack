9e5f9aea328a24f5bbb3ca9139c668cd6391727e
Jeremy Goh <jg485host@gmail.com>
2013-08-20 15:20:52 +0100

    add association and validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
 has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :square => "250>x250#", :large => "500x500>"}, :processors => [:cropper]
 
 has_many :comments
+belongs_to :user
+
+validates_presence_of :user_id
 
 	before_create do |post|
      post.time = 86400
