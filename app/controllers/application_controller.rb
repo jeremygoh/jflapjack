353a76c922d9c94f30949d968376c20883d68637
Jeremy Goh <jg485host@gmail.com>
2013-08-20 17:30:51 +0100

    added filters for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def redirect_if_not_signed_in
+    if !current_user
+      redirect_to "/users/sign_in" unless current_user
+      flash[:alert] = "Please register or sign in"
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
@@ -11,5 +18,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
+
 
 end
