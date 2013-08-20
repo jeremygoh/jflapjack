5040e8f24c71055d4951cd615a9e4e7471e66900
Jeremy Goh <jg485host@gmail.com>
2013-08-20 15:46:19 +0100

    added user reference and db validation for post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820144120) do
+ActiveRecord::Schema.define(version: 20130820144451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 20130820144120) do
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
     t.integer  "time"
+    t.integer  "user_id",            null: false
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
