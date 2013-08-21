66da101d14d61f7797645475b63b28c2ccddca56
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-21 09:23:35 +0100

    removed pc helper

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,27 +1,3 @@
-require 'debugger'
-
 module PhotosHelper
 end
 
-module Paperclip
-  class Cropper < Thumbnail
-    def transformation_command
-      if crop_command
-        commands = super
-
-        commands.delete_if { |command| command.include? 'crop' }
-        [crop_command] + commands
-      else
-        super
-      end
-    end
-    
-    def crop_command
-      target = @attachment.instance
-      if target.cropping?
-        # debugger
-        " -crop '#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}'"
-      end
-    end
-  end
-end
