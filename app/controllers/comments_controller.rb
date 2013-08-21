d1ee2214dfee9a0d53c6568684a725af642d75d8
Jeremy Goh <jg485host@gmail.com>
2013-08-21 14:14:37 +0100

    ajax post for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,8 @@ before_action :redirect_if_not_signed_in
     comment = Comment.new(params[:comment].permit(:post_id, :body))
     comment.user = current_user
     if comment.save
-      flash[:notice] = "Comment saved successfully"
-      websocket[:comments].trigger 'new',{id: comment.id, body: comment.body}
+      # flash[:notice] = "Comment saved successfully"
+      websocket[:comments].trigger 'new',{post_id: comment.post.id, id: comment.id, body: comment.body}
      else
       flash[:alert] = "Failed to save comment"
     end
