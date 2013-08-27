34780e0339aa2dfc886a63d518f3580c6d9a8f6c
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-27 21:43:47 +0100

    add youtube thumbs

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,9 @@
 module VideosHelper
+
+	def youtube_thumb_url(url, num)
+		# http://www.youtube.com/watch?v=_eeFb8skrV0
+		# http://img.youtube.com/vi/_eeFb8skrV0/1.jpg
+		regex = /^(?:http:\/\/)?(?:www\.)?\w*\.\w*\/(?:watch\?v=)?((?:p\/)?[\w\-]+)/
+		youtube_thumb_url="http://img.youtube.com/vi/" + url.match(regex)[1] + "/" + num.to_s + ".jpg"
+	end
 end
