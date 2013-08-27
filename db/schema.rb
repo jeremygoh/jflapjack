5f9ad3340a9b56f179f0bdddcc91d0d325fcd330
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-27 17:18:02 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827104200) do
+ActiveRecord::Schema.define(version: 20130827160212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20130827104200) do
     t.integer  "user_id",            null: false
     t.string   "thumbnail"
     t.string   "youtube_url"
+    t.boolean  "cropped"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
