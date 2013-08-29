11213ae851725afce8ca06167dd67caf54603ae9
Jeremy Goh <jg485host@gmail.com>
2013-08-29 12:46:19 +0100

    follow and unfollow work for ajax and page load panel comments. Need to make follow/unfollow links dynamic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,36 @@ include PostsHelper
 class PostsController < ApplicationController
   
   def index
+
+    # render text: "No posts" and return if current_
+
     @posts = []
     if current_user   
       @followed_users_ids_in_json = current_user.followers.to_json
       followed_posts = current_user.followed_posts
-      if !followed_posts.empty?
-        @posts = followed_posts
-        # @posts = Post.followed_by(current_user).order("updated_at desc")
+
+      #check if there are any followed posts or that you have created a post 
+      if !followed_posts.empty? || current_user.posts.size > 0 
+        @posts = followed_posts 
+        current_user.posts.each do |post|
+            @posts << post
+        end        # @posts = Post.followed_by(current_user).order("updated_at desc")
+     
       else
         flash[:notice] = "These are the top 5 site users posts. Maybe follow 1 or 2 to get started..."
         get_top_five_users_posts.flatten.each do |post| #replace with top 5 users
+          @posts << post
+        end
+        current_user.posts.each do |post|
           @posts << post
         end
       end
     else
         @posts = Post.find(:all, :order => "id desc")
     end
-    @posts.keep_if{|post| get_size_multiplier(post) > 0.3 }       ###!!!!!!THIS CONTROLS WHETHER A POST IS DISPLAYED BASED ON SIZE!!!!!!!
+
+
+    @posts.keep_if{|post| get_size_multiplier(post) > 0.3 } unless @posts.empty?      ###!!!!!!THIS CONTROLS WHETHER A POST IS DISPLAYED BASED ON SIZE!!!!!!!
     
     @last_comments = Comment.find(:all, :order => "created_at desc", :limit => 10)
   end
