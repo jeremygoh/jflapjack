9cb70233a7ba09268b3027566354de773e77a8d5
Jeremy Goh <jg485host@gmail.com>
2014-02-23 10:06:56 -0800

    added youtube thumbnail fix for https

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,7 +1,7 @@
 module VideosHelper
 
 	def youtube_thumb_url(url, num)
-		regex = /^(?:http:\/\/)?(?:www\.)?\w*\.\w*\/(?:watch\?v=)?((?:p\/)?[\w\-]+)/
+		regex = /^(?:http:\/\/)?(?:https:\/\/)?(?:www\.)?\w*\.\w*\/(?:watch\?v=)?((?:p\/)?[\w\-]+)/
 		youtube_thumb_url="http://img.youtube.com/vi/" + url.match(regex)[1] + "/" + num.to_s + ".jpg"
 	end
 end
